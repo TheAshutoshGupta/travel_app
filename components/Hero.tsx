28cd1b1714e4dc90b40f8afcddca9fac7ce06ed6
Ashutosh Gupta <ashutoshg479@gmail.com>
2023-10-16 14:20:36 +0530

    config

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -34,11 +34,13 @@ const Hero = () => {
                 </p>
             </div>
             <div className="flex flex-col w-full gap-3 sm:flex-row">
-                <Button type="button" 
+                <Button 
+                type="button" 
                 title="Download App"
                 variant="btn_green"/>
 
-                <Button type="button" 
+                <Button 
+                type="button" 
                 title="How we work?"
                 icon="/play.svg"
                 variant="btn_white_text"/>
